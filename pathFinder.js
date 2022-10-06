9306dcf97c408cd22934eff1fab40e82d646d23b
Franck Hourdin <fhourdin@ftel.fr>
2022-10-06 10:30:17 +0200

    ✨ Handle getLegalMoves

--- pathFinder.js
+++ pathFinder.js
@@ -1,7 +1,66 @@
+const BOARD_SIZE = 8;
 class PathFinder {
-    getMinimumMoves = (position, target) => {
-        return 0
+  left = (position) => {
+    if (position === -1) {
+      return -1;
     }
+
+    if (position % BOARD_SIZE === 1) {
+      return -1;
+    }
+
+    return position - 1;
+  };
+
+  right = (position) => {
+    if (position === -1) {
+      return -1;
+    }
+    if (position % BOARD_SIZE === 0) {
+      return -1;
+    }
+
+    return position + 1;
+  };
+
+  up = (position) => {
+    if (position === -1) {
+      return -1;
+    }
+    if (position / BOARD_SIZE <= 1) {
+      return -1;
+    }
+
+    return position - BOARD_SIZE;
+  };
+
+  down = (position) => {
+    if (position === -1) {
+      return -1;
+    }
+    if (position / BOARD_SIZE > BOARD_SIZE - 1) {
+      return -1;
+    }
+
+    return position + BOARD_SIZE;
+  };
+
+  getLegalMoves = (position) => {
+    return [
+      this.up(this.up(this.left(position))),
+      this.up(this.up(this.right(position))),
+      this.right(this.right(this.up(position))),
+      this.right(this.right(this.down(position))),
+      this.down(this.down(this.right(position))),
+      this.down(this.down(this.left(position))),
+      this.left(this.left(this.down(position))),
+      this.left(this.left(this.up(position))),
+    ].filter((move) => move !== -1);
+  };
+
+  getMinimumMoves = (position, target) => {
+    return 0;
+  };
 }
 
 module.exports = PathFinder;
--- pathFinder.test.js
+++ pathFinder.test.js
@@ -1,26 +1,104 @@
-const PathFinder = require('./pathFinder');
+const { isEqual, sortBy } = require("lodash");
+const PathFinder = require("./pathFinder");
 
-test('From 1 to 1', () => {
+describe("getMinimumMoves()", () => {
+  test("From 1 to 1", () => {
     const finder = new PathFinder();
     expect(finder.getMinimumMoves(1, 1)).toBe(0);
-});
+  });
 
-test('From 19 to 53', () => {
+  test("From 19 to 53", () => {
     const finder = new PathFinder();
     expect(finder.getMinimumMoves(19, 53)).toBe(2);
-});
+  });
 
-test('From 1 to 64', () => {
+  test("From 1 to 64", () => {
     const finder = new PathFinder();
     expect(finder.getMinimumMoves(1, 64)).toBe(6);
-});
+  });
 
-test('From 50 to 20', () => {
+  test("From 50 to 20", () => {
     const finder = new PathFinder();
     expect(finder.getMinimumMoves(50, 20)).toBe(2);
-});
+  });
 
-test('From 50 to 51', () => {
+  test("From 50 to 51", () => {
     const finder = new PathFinder();
     expect(finder.getMinimumMoves(50, 51)).toBe(3);
+  });
+});
+
+describe("left()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    expect(finder.left(20)).toBe(19);
+  });
+  test("From 9", () => {
+    const finder = new PathFinder();
+    expect(finder.left(9)).toBe(-1);
+  });
+});
+
+describe("right()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    expect(finder.right(20)).toBe(21);
+  });
+  test("From 16", () => {
+    const finder = new PathFinder();
+    expect(finder.right(16)).toBe(-1);
+  });
+});
+
+describe("up()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    expect(finder.up(20)).toBe(12);
+  });
+  test("From 5", () => {
+    const finder = new PathFinder();
+    expect(finder.up(5)).toBe(-1);
+  });
+});
+
+describe("up()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    expect(finder.up(20)).toBe(12);
+  });
+  test("From 61", () => {
+    const finder = new PathFinder();
+    expect(finder.up(5)).toBe(-1);
+  });
+});
+
+describe("down()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    expect(finder.down(20)).toBe(28);
+  });
+  test("From 61", () => {
+    const finder = new PathFinder();
+    expect(finder.down(61)).toBe(-1);
+  });
+});
+
+describe("getLegalMoves()", () => {
+  test("From 20", () => {
+    const finder = new PathFinder();
+    const arrayEquality = isEqual(
+      sortBy(finder.getLegalMoves(20)),
+      [3, 5, 10, 14, 26, 30, 35, 37]
+    );
+    expect(arrayEquality).toBeTruthy();
+  });
+  test("From 61", () => {
+    const finder = new PathFinder();
+    console.log(finder.getLegalMoves(61));
+    const arrayEquality = isEqual(
+      sortBy(finder.getLegalMoves(61)),
+      [44, 46, 51, 55]
+    );
+    expect(arrayEquality).toBeTruthy();
+  });
 });
