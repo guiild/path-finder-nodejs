fd02fd1730c106376c196671111fe65ff8c766c1
jjdup <jeremi+github@dupin.fdn.fr>
2023-04-03 16:41:13 +0200

    implemantation

--- pathFinder.js
+++ pathFinder.js
@@ -3,10 +3,40 @@ class PathFinder {
     constructor(position, target) {
         this.position = position;
         this.target = target;
+        this.edge = 8;
     }
 
     getMinimumMoves = () => {
-        return 0
+        if (this.is_diagonal_2) {
+            return 4;
+	} else if (this.is_near) {
+            return 3;
+	}
+        return this.mp + ((this.mp + this.dx + this.dy) % 2)
+    }
+
+    get is_diagonal_2() {
+        return (this.dx == 2) && (this.dy == 2)
+    }
+
+    get is_near() {
+        return (this.dx + this.dy) == 1
+    }
+
+    get mp() {
+	return Math.round(Math.max(
+            this.dx / 2,
+            this.dy / 2,
+            (this.dx + this.dy) / 3
+	));
+    }
+
+    get dx() {
+        return Math.abs(((this.position-1) % this.edge) - ((this.target-1) % this.edge));
+    }
+
+    get dy() {
+        return Math.abs(Math.floor((this.position-1) / this.edge) - Math.floor((this.target-1) / this.edge));
     }
 
 }
