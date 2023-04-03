62455013c9a683da701af379cc567b7b1717a3fd
jjdup <jeremi+github@dupin.fdn.fr>
2023-04-03 21:06:26 +0200

    refactor: introduce map class

--- pathFinder.js
+++ pathFinder.js
@@ -4,9 +4,9 @@ class PathFinder {
 
     constructor(actual, target, edge = 8) {
 	assert(edge >= 5, 'Minimum edge size is 5');
-        this.edge = edge;
-        this.from = new Position(this.edge, actual)
-        this.to = new Position(this.edge, target)
+        this.from = new Position(edge, actual)
+        this.to = new Position(edge, target)
+        this.map = new Map(this.from, this.to)
     }
 
     getMinimumMoves = () => {
@@ -17,21 +17,29 @@ class PathFinder {
 	} else if (this.is_corner_special_case) {
             return 4;
         }
-        return this.mp + ((this.mp + this.dx + this.dy) % 2)
+        return this.map.mp + ((this.map.mp + this.map.dx + this.map.dy) % 2)
     }
 
     get is_diagonal_special_case() {
-        return (this.dx == 2) && (this.dy == 2)
+        return (this.map.dx == 2) && (this.map.dy == 2)
     }
 
     get is_near_special_case() {
-        return (this.dx + this.dy) == 1
+        return (this.map.dx + this.map.dy) == 1
     }
 
     get is_corner_special_case() {
         return this.from.is_in_corner
-	    && this.dx == 1
-	    && this.dy == 1
+	    && this.map.dx == 1
+	    && this.map.dy == 1
+    }
+}
+
+class Map {
+
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
     }
 
     get mp() {
@@ -49,7 +57,6 @@ class PathFinder {
     get dy() {
         return Math.abs(this.from.y - this.to.y);
     }
-
 }
 
 class Position {
